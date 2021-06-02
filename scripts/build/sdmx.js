f0f5cc10268eff3f6d867ad91f63040e31dc0bf1
Brock Fanning <brockfanning@gmail.com>
2021-06-02 18:52:43 -0400

    Custom sdmx generation

--- scripts/build/sdmx.js
+++ scripts/build/sdmx.js
@@ -1,64 +1,54 @@
 module.exports = function(refresh=false) {
 
     const path = require('path')
-    const fs = require('fs')
     const utils = require('./utils')
     const store = require('../translation-store')
 
     const sdgMetadataConvert = require('sdg-metadata-convert')
     const sdmxOutput = new sdgMetadataConvert.SdmxOutput()
-    const wordTemplateInput = new sdgMetadataConvert.WordTemplateInput({
-        debug: false,
-        cheerio: { decodeEntities: false },
-    })
 
     if (refresh) {
         store.refresh()
     }
     const translations = store.getTranslationStore()
 
-    const sourceFolder = 'indicators'
-    const extensions = ['.docx', '.docm']
-    const files = fs.readdirSync(sourceFolder).filter(file => {
-        return extensions.includes(path.extname(file).toLowerCase());
-    })
-    const conversions = files.map(sourceFile => {
-        const sourcePath = path.join(sourceFolder, sourceFile)
-        const indicatorId = path.parse(sourceFile).name
-        const targetFile = indicatorId + '.xml'
-        return [sourcePath, targetFile, indicatorId]
-    })
-
-    function prepareOutput(text) {
-        return text
-            .replace(/'/g, '&#39;')
-    }
-
     convertSdmx()
 
+    function getSeriesFromIndicatorId(indicatorId) {
+        indicatorId = indicatorId.replace(/-/g, '.')
+        const series = sdgMetadataConvert.descriptorStore.getDescriptor('SERIES')
+        const matches = series.options.filter(item => item.indicatorId === indicatorId)
+        if (matches.length === 0) {
+            return false
+        }
+        if (matches.length > 1) {
+            console.log('WARNING: Multiple series matching indicator ' + indicatorId)
+            console.log('-- (using the first one)')
+        }
+        return matches[0].indicatorId
+    }
+
     async function convertSdmx() {
-        for (const conversion of conversions) {
-            const [sourcePath, targetFile, indicatorId] = conversion
-            try {
-                const metadata = await wordTemplateInput.read(sourcePath)
-                const series = metadata.getDescriptor('SERIES')
-                if (!series || series.length === 0) {
+        for (const language of Object.keys(translations)) {
+            for (const indicatorId of Object.keys(translations[language])) {
+                const concepts = translations[language][indicatorId]
+                const series = getSeriesFromIndicatorId(indicatorId)
+                if (!series) {
                     console.log('Unable to produce SDMX for ' + indicatorId + '. SERIES could not be identified.')
                     continue
                 }
-                for (const language of store.getLanguages()) {
-                    metadata.setDescriptor('LANGUAGE', language)
-                    for (const concept of Object.keys(metadata.getConcepts())) {
-                        const value = translations[language][indicatorId][concept]
-                        metadata.setConcept(concept, prepareOutput(value))
-                    }
-                    const targetFolder = utils.createFolder(['www', 'sdmx', language])
-                    const targetPath = path.join(targetFolder, targetFile)
-                    await sdmxOutput.write(metadata, targetPath)
-                    console.log(`Created ${targetPath}.`);
+                const descriptors = {
+                    'LANGUAGE': language,
+                    'REPORTING_TYPE': 'N',
+                    'REF_AREA': 'BI',
+                    'SERIES': series,
                 }
-            } catch(e) {
-                console.log(e)
+                const metadata = new sdgMetadataConvert.Metadata(concepts, descriptors)
+                const targetFolder = utils.createFolder(['www', 'sdmx', language])
+                const targetFile = indicatorId + '.xml'
+                const targetPath = path.join(targetFolder, targetFile)
+                await sdmxOutput.write(metadata, targetPath)
+                console.log(`Created ${targetPath}.`);
             }
         }
     }
